b11753d6d96a7c39603178cf0239f834c17e9c5d
AFCMS <afcm.contact@gmail.com>
2025-08-28 00:10:36 +0200

    Harder ESLint config

--- src/main.tsx
+++ src/main.tsx
@@ -6,6 +6,7 @@ import App from "./App.tsx";
 
 import "./index.css";
 
+// eslint-disable-next-line @typescript-eslint/no-non-null-assertion
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <Analytics />
