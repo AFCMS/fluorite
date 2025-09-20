7c1719ad79d112ddd3bd43d5d98be3d452e988ca
Klhmt <78567345+Klhmt@users.noreply.github.com>
2025-09-20 22:07:29 +0200

    automatically use browser language

--- src/main.tsx
+++ src/main.tsx
@@ -8,8 +8,11 @@ import App from "./App.tsx";
 
 import "./index.css";
 import { dynamicActivate } from "./utils/i18nLocaleModuleLoading.ts";
+import { getSanitizedBrowserLanguageIfSupportedOrEnglish } from "./utils/getSanitizedBrowserLanguageIfSupportedOrEnglish.ts";
 
-await dynamicActivate("fr");
+const sanitizedBrowserLanguageIfSupportedOrEnglish =
+  getSanitizedBrowserLanguageIfSupportedOrEnglish();
+await dynamicActivate(sanitizedBrowserLanguageIfSupportedOrEnglish);
 
 // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
 createRoot(document.getElementById("root")!).render(
