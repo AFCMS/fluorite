ffd03446148399f15ffda9802e1f702510bef563
Klhmt <78567345+Klhmt@users.noreply.github.com>
2025-09-20 21:10:08 +0200

    fix broken import path

--- src/utils/i18nLocaleModuleLoading.ts
+++ src/utils/i18nLocaleModuleLoading.ts
@@ -2,7 +2,7 @@ import { i18n } from "@lingui/core";
 
 export async function dynamicActivate(locale: string) {
   // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
-  const { messages } = await import(`./../locales/${locale}.po`);
+  const { messages } = await import(`./../locales/${locale}/messages.po`);
 
   // eslint-disable-next-line @typescript-eslint/no-unsafe-call, @typescript-eslint/no-unsafe-member-access
   i18n.load(locale, messages);
