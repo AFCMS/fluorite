dc79557760b51773add1d8af8e4f05881b3b640c
Klhmt <78567345+Klhmt@users.noreply.github.com>
2025-09-21 13:02:31 +0200

    add eslint plugin for lingui

--- eslint.config.js
+++ eslint.config.js
@@ -6,6 +6,7 @@ import tseslint from "typescript-eslint";
 import { globalIgnores } from "eslint/config";
 
 export default tseslint.config([
+  pluginLingui.configs["flat/recommended"],
   globalIgnores(["dist"]),
   {
     files: ["**/*.{ts,tsx}"],
