28fa9d74432f471ad930ab8b82b10e673fefd7d0
Klhmt <78567345+Klhmt@users.noreply.github.com>
2025-09-21 13:31:26 +0200

    fix CI error

--- eslint.config.js
+++ eslint.config.js
@@ -4,6 +4,7 @@ import reactHooks from "eslint-plugin-react-hooks";
 import reactRefresh from "eslint-plugin-react-refresh";
 import tseslint from "typescript-eslint";
 import { globalIgnores } from "eslint/config";
+import pluginLingui from "eslint-plugin-lingui";
 
 export default tseslint.config([
   pluginLingui.configs["flat/recommended"],
--- src/utils/i18nLocaleModuleLoading.ts
+++ src/utils/i18nLocaleModuleLoading.ts
@@ -1,7 +1,9 @@
-import { i18n } from "@lingui/core";
+import { i18n, type Messages } from "@lingui/core";
 
 export async function dynamicActivate(locale: string) {
-  const { messages } = await import(`./../locales/${locale}/messages.po`);
+  const { messages } = (await import(`./../locales/${locale}/messages.po`)) as {
+    messages: Messages;
+  };
 
   i18n.load(locale, messages);
   i18n.activate(locale);
