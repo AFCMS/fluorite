27fcae6fa8609921af062ac86aee6a0a9431f1d2
AFCMS <afcm.contact@gmail.com>
2025-10-12 16:28:19 +0200

    Switch Prettier config to TypeScript

--- prettier.config.ts
+++ prettier.config.ts
@@ -1,8 +1,9 @@
+import { type Config } from "prettier";
+
 /**
  * @see https://prettier.io/docs/en/configuration.html
- * @type {import("prettier").Config}
  */
-const config = {
+const config: Config = {
   plugins: ["prettier-plugin-tailwindcss"],
   overrides: [
     {
